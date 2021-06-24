8fd39699f544dd4c9eca9bfbe791c26f3989530a
chem <chem@fb.com>
2021-06-23 18:08:40 -0700

    Onclick intent complete.

---
+++ app/src/main/java/com/codepath/cchem/flixster/adapters/MovieAdapter.java
@@ -0,0 +1,117 @@
+package com.codepath.cchem.flixster.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.util.Log;
+import android.view.LayoutInflater;
+import  android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.FitCenter;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.codepath.cchem.flixster.MovieDetailsActivity;
+import com.codepath.cchem.flixster.R;
+import com.codepath.cchem.flixster.models.Movie;
+
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+    Context context;
+    List<Movie> movies;
+    String TAG = "MovieAdapter";
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    // Inflating a layout from XML and returning the holder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder");
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(movieView);
+    }
+
+    // Populating data into the item through holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder "+ position);
+        // Get the movie at the passed position
+        Movie movie = movies.get(position);
+        // Bind the movie data into the VH
+        holder.bind(movie);
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster= itemView.findViewById(R.id.ivPoster);
+            // add as the itemView's OnClickListener
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(Movie movie){
+            int radius = 10;
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+
+            String imageUrl;
+            // Image url changes based on portrait or landscape modes
+            if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                imageUrl = movie.getBackdropPath();
+            } else {
+                imageUrl = movie.getPosterPath();
+            }
+
+            Glide.with(context)
+                    .load(imageUrl)
+                    .transform(new FitCenter(),new RoundedCorners(25))
+                    .placeholder(R.drawable.placeholder).transform(new FitCenter(),new RoundedCorners(25))
+                    .into(ivPoster);
+
+        }
+
+        // when the user clicks on a row, show MovieDetailsActivity for the selected movie
+        @Override
+        public void onClick(View v) {
+            // gets item position
+            int position = getAdapterPosition();
+            // make sure the position is valid, i.e. actually exists in the view
+            if (position != RecyclerView.NO_POSITION) {
+                // get the movie at the position, this won't work if the class is static
+                Movie movie = movies.get(position);
+                // create intent for the new activity
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                // serialize the movie using parceler, use its short name as a key
+                intent.putExtra(Movie.class.getSimpleName(), Parcels.wrap(movie));
+                // show the activity
+                context.startActivity(intent);
+            }
+        }
+    }
+}
